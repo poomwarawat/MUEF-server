b14867dc3bf6c435d6de07681b135cce2dad0207
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-05-26 02:46:04 +0700

    Update upload csv to database

--- route/admin.js
+++ route/admin.js
@@ -59,4 +59,63 @@ router.post("/search-student", (req, res) => {
   });
 });
 
+router.post("/upload-csv-data", (req, res) => {
+  var count = 0;
+  for (let index = 1; index <= req.body.LengthData; index++) {
+    const value = `newstd${index}`;
+    const stringValue = value.toString();
+    const data = req.body[stringValue];
+    const splitData = data.split(",");
+    const currentDate = new Date();
+    const getDate = currentDate.getDate();
+    const getDay = currentDate.getDay();
+    const getFullYear = currentDate.getFullYear();
+    const getMilli = currentDate.getMilliseconds();
+    const getHours = currentDate.getHours();
+    const genID = makeid(10);
+    const codeID = `${getFullYear}${getDay}${getDate}${getHours}${getMilli}${genID}`;
+    const sql = `INSERT INTO allstudent 
+    (fname, lname, nname, gender, birthday, salary, stdCode, stdRoom, 
+      schoolname, region, district, province, codeId, user) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const Data = [
+      splitData[1],
+      splitData[2],
+      splitData[3],
+      splitData[4],
+      splitData[5],
+      splitData[6],
+      splitData[7],
+      splitData[8],
+      splitData[9],
+      splitData[10],
+      splitData[11],
+      splitData[12],
+      codeID,
+      req.body.username,
+    ];
+    const values = Object.values(Data);
+    con.query(sql, values, (err, result) => {
+      if (err) throw err;
+      if (result) {
+        count += 1;
+        if (count === parseInt(req.body.LengthData)) {
+          return res.status(200).send({ add: true });
+        }
+      }
+    });
+  }
+});
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 module.exports = router;
