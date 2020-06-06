4183dccbd2a8a75b78fe7a74cf79c4799249010d
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-06-07 01:21:24 +0700

    config port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use("/", Question);
 app.use("/", scoreResult);
 app.use("/admin", adminRoute);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
